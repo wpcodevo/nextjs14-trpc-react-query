3cf628d9e2fec5e9c5e67dc39eb579582863690d
Edem <contact@codevoweb.com>
2024-01-18 18:41:39 +0000

    setup trpc in the app router

---
+++ app/api/trpc/trpc-router.ts
@@ -0,0 +1,17 @@
+import { initTRPC } from '@trpc/server';
+import superjson from 'superjson';
+
+const t = initTRPC.create({
+  transformer: superjson,
+});
+
+export const appRouter = t.router({
+  healthchecker: t.procedure.query(({ ctx }) => {
+    return {
+      status: 'success',
+      message: 'Welcome to trpc with Next.js 14 and React Query',
+    };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
---
+++ utils/query-client.ts
@@ -0,0 +1,6 @@
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 5 * 1000 } },
+});
+export default queryClient;
